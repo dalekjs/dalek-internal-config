9719e4e664afdd03bcfdb4e0df947ff9e4e1ffec
asciidisco <public@asciidisco.com>
2013-08-01 21:56:52 +0200

    Lint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,11 +20,6 @@ module.exports = function (grunt) {
       coverage: ['coverage', 'report/coverage'],
       report: ['report/complexity', 'report/api', 'report/docs'],
       reportZip: ['report.zip'],
-        vars: {
-          title: 'DalekJS - Documentation - Config',
-          desc: 'DalekJS - Documentation - Config',
-          docs: true
-        }
     },
 
     // linting
